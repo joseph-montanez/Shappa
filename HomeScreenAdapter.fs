fd5497e54226b5ade83e179c5ecfb5051aebf415
Joseph Montanez <jm@shabb.com>
2014-06-17 08:45:03 -0700

    init

---
+++ HomeScreenAdapter.fs
@@ -0,0 +1,28 @@
+﻿
+namespace Shappa
+open System
+open System.Collections
+open System.Collections.Generic
+open System.Collections.ObjectModel
+open Android.App
+open Android.Content
+open Android.OS
+open Android.Runtime
+open Android.Views
+open Android.Widget
+
+
+
+type HomeScreenAdapter(context : Activity, items : array<string>) = 
+    inherit BaseAdapter ()
+    let context = context
+
+    override this.Count with get () = items.Length
+    override this.GetItem(position) = new Java.Lang.String(items.[position].ToCharArray()) :> Java.Lang.Object
+    override this.GetItemId(position) = int64 position
+    override this.GetView(position, convertView, parent) =
+        let view = match convertView with
+                    | null -> context.LayoutInflater.Inflate(Resource_Layout.SimpleListItem1, null)
+                    | _ -> convertView
+        view.FindViewById<TextView>(Resource_Id.txtview).Text <- items.[position].ToString()
+        view
---
+++ MainActivity.fs
@@ -0,0 +1,38 @@
+﻿namespace Shappa
+
+open System
+
+open Android.App
+open Android.Content
+open Android.OS
+open Android.Runtime
+open Android.Views
+open Android.Widget
+
+[<Activity (Label = "Shappa", MainLauncher = true)>]
+type MainActivity () =
+    inherit ListActivity ()
+
+    let mutable count = 1
+    let items = [| "Orders"; "Materials" |]
+
+
+    override this.OnCreate (bundle) =
+
+        base.OnCreate (bundle)
+        this.SetContentView (Resource_Layout.Main)
+
+        // Add array to list view
+        this.ListAdapter <- new HomeScreenAdapter(this, items)
+
+        // Add click event to the button
+        let button = this.FindViewById<Button>(Resource_Id.myButton)
+        button.Click.Add (fun args -> 
+            button.Text <- sprintf "%d clicks!" count
+            count <- count + 1
+        )
+
+    override this.OnListItemClick (listView, view, position, id) = 
+        let item = items.[position]
+        let toast = Toast.MakeText(this, item, ToastLength.Short)
+        toast.Show()
